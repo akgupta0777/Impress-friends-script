165bc19bf2f315c417b91bb7f257db5484f009be
akgupta0777 <akgupta0777@gmail.com>
2020-11-28 14:49:01 +0530

    Added javaScript

---
+++ x.js
@@ -0,0 +1,52 @@
+const readline = require('readline');
+
+console.log("WELCOME ABHAY");
+console.log("Software Name : HACKINTOSH");
+console.log("version info : 14.1.0");
+console.log("Build Status : PASSING");
+console.log("Secure Authentication Process Started");
+
+
+var user = "AbhayGupta0777";
+var pass = "***********";
+console.log("Username:");
+setTimeout(() => console.log(user) , 3000);
+console.log(user);
+console.log("Password:");
+setTimeout(() => console.log(pass) , 6000);
+console.log(pass);
+
+console.log("AUTHENTICATION SUCCESSFUL ! HACKING DATABASE IN A SECOND");
+//for(let i=0;i<=100;i++){
+//console.log("Hacking into Database "+i+"% done");
+//}
+setTimeout(() => {console.log("HACK INITIATED!");
+
+console.log("SWITCHING OFF REMOTE CONNECTIONS!");
+
+console.log("READY TO INJECT SQL INJECTION ATTACK");
+
+console.log("APPLYING PATCHES SECURELY");
+
+} ,10000);
+
+console.log(pass);
+console.log(" ");
+console.log(" ");
+setTimeout(() => {
+console.log(" ");
+console.log(" ");
+console.log("SOMEONE IS TRACKING US!! WARNING : CODE RED!");
+console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DANGER WARNING !!!!!!!!!!!!!!!!!!!!!!!!!!");
+console.log("BLOCKING OTHER DENIAL OF SERVICE TO ENSURE YOUR SECURITY");
+
+console.log("THEY ARE TRACKING OUR LOCATION ! FAKE ACCESS POINT FIRED UP");
+
+console.log("LOCATION CHANGED TO MELROSE PARK IL US");},20000);
+
+setTimeout(() => {
+console.log("=================================================");
+console.log("DATABASE HACKED !! ");
+console.log("ACCESS GRANTED");
+console.log("WE ARE IN THE SYSTEM DO WHATEVER YOU WANT");
+},30000);
